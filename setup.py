7b1c0c32439e11cafa24807dcb439ad273708188
Michael T Smith <lionfishy@gmail.com>
2016-02-25 16:38:02 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pynbn',
+      version='0.1',
+      description='Access to NBN data from python',
+      url='https://github.com/lionfish0/pynbn',
+      author='Mike Smith',
+      author_email='mike@michaeltsmith.org.uk',
+      license='MIT',
+      packages=['pynbn'],
+      zip_safe=False)
