4bd2c9f644eaf3b7fd875a2fdef6490d21f1c311
Michael T Smith <lionfishy@gmail.com>
2016-08-08 15:49:19 +0100

    v1.0.1: Added install requires to setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
 setup(name='pynbn',
-      version='1.0.0',
+      version='1.0.1',
       description='Access to NBN data from python',
       url='https://github.com/lionfish0/pynbn',
       author='Mike Smith',
       author_email='mike@michaeltsmith.org.uk',
       license='MIT',
       packages=['pynbn'],
+      keywords='NBN access wrapper',
+      install_requires=['requests'],
       zip_safe=False)
